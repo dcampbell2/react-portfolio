25a79ef7e8cfad180b0ed52fa5f2bcfa3ed694bd
Dame <dcampbell7095@gmail.com>
2021-01-30 09:29:59 -0500

    wrapping routes in browserrouter

--- src/App.js
+++ src/App.js
@@ -3,25 +3,31 @@ import Nav from "./components/Nav/Nav";
 import Contact from "./containers/Contact/Contact";
 import Portfolio from "./containers/Portfolio/Portfolio";
 import projects from "./projects.json";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  BrowserRouter,
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from "react-router-dom";
 function App() {
   return (
-    <div>
-      <Router>
-        <Nav />
-        <Switch>
-          <Route path="/Contact" component={Contact} />
-          <Route
-            path="/Portfolio"
-            render={(project) => (
-              <Portfolio {...project} image={projects[0].image} />
-            )}
-          />
-          <Route path="*" component={Home} />
-        </Switch>
-      </Router>
-      
-    </div>
+    <BrowserRouter>
+      <div>
+        <Router>
+          <Nav />
+          <Switch>
+            <Route path="/Contact" component={Contact} />
+            <Route
+              path="/Portfolio"
+              render={(project) => (
+                <Portfolio {...project} image={projects[0].image} />
+              )}
+            />
+            <Route path="*" component={Home} />
+          </Switch>
+        </Router>
+      </div>
+    </BrowserRouter>
   );
 }
 
