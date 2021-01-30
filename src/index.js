5dbc8711d09b32c14832cd9e3ab589a28ad89188
Dame <dcampbell7095@gmail.com>
2021-01-30 09:32:46 -0500

    adding hashrouter to index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import { HashRouter } from "react-router-dom";
 import App from "./App";
 
 ReactDOM.render(
-    <App />,
+  <HashRouter>
+    <App />
+  </HashRouter>,
   document.getElementById("root")
 );
