eecb35a7168c22f9638d4e29ae52d01f4dc97260
Dame <dcampbell7095@gmail.com>
2021-02-01 07:37:09 -0500

    fixing classes in portfolio.jsx

--- src/containers/Portfolio/Portfolio.jsx
+++ src/containers/Portfolio/Portfolio.jsx
@@ -3,21 +3,21 @@ import React from "react";
 const Portfolio = (props) => {
   return (
     <div>
-      <div class="container">
-        <div class="row">
-          <div class="col-sm-8">
-            <div class="jumbotron">
-              <h1 class="display-4">Portfolio</h1>
-              <hr class="my-4" />
-              <div class="row images">
-                <div class="col-sm-6">
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-8">
+            <div className="jumbotron">
+              <h1 className="display-4">Portfolio</h1>
+              <hr className="my-4" />
+              <div className="row images">
+                <div className="col-sm-6">
                   <a
                     href="https://dcampbell2.github.io/covid-cost/"
                     target="_blank"
                   >
                     <img
                       src={props.image}
-                      class="img-fluid covid-cost"
+                      className="img-fluid covid-cost"
                       alt="Covid Cost Project-1"
                     />
                   </a>
@@ -25,17 +25,17 @@ const Portfolio = (props) => {
                     href="https://github.com/dcampbell2/covid-cost"
                     target="_blank"
                   >
-                    <span class="text-justify">{props.name}</span>
+                    <span className="text-justify">{props.name}</span>
                   </a>
                 </div>
-                <div class="col-sm-6">
+                <div className="col-sm-6">
                   <a
                     href="https://dcampbell2.github.io/password-generator/"
                     target="_blank"
                   >
                     <img
                       src="./assets/Images/Generate-Password.png"
-                      class="img-fluid"
+                      className="img-fluid"
                       alt="Password Generator"
                     />
                   </a>
@@ -43,19 +43,19 @@ const Portfolio = (props) => {
                     href="https://github.com/dcampbell2/password-generator"
                     target="_blank"
                   >
-                    <span class="text-justify">Password Generator Repo</span>
+                    <span className="text-justify">Password Generator Repo</span>
                   </a>
                 </div>
-                <div class="container">
-                  <div class="row">
-                    <div class="col-sm-6">
+                <div className="container">
+                  <div className="row">
+                    <div className="col-sm-6">
                       <a
                         href="https://dcampbell2.github.io/weather-search-dashboard/"
                         target="_blank"
                       >
                         <img
                           src="./assets/Images/weather-dashboard.png"
-                          class="img-fluid"
+                          className="img-fluid"
                           alt="Weather Dashboard"
                         />
                       </a>
@@ -63,7 +63,7 @@ const Portfolio = (props) => {
                         href="https://github.com/dcampbell2/weather-search-dashboard"
                         target="_blank"
                       >
-                        <span class="text-justify">Weather Dashboard Repo</span>
+                        <span className="text-justify">Weather Dashboard Repo</span>
                       </a>
                     </div>
                   </div>
